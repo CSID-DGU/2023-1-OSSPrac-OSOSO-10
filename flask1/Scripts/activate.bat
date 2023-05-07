6f6e0cce1771b2ebe5252ac570fab146412460fc
HyeounChoi-hyon <2021110765@dgu.ac.kr>
2023-05-07 18:33:28 +0900

    OSOSO_최종 완성 코드

--- flask1/Scripts/activate.bat
+++ flask1/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\2023-1-OSSPrac-OSOSO-10\flask1
+set VIRTUAL_ENV=C:\Users\user\2023-1-OSSPrac-OSOSO-10\2023-1-OSSPrac-OSOSO-10\flask1
 
 if not defined PROMPT set PROMPT=$P$G
 
